83c5f2cd170294e598fd3a99a64c23c70d508386
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-01-09 16:03:42 +0100

    ADD: implementazione cors

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express");
 const moviesRouter = require("./routes/movies");
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors()); // Aggiungi questa riga per il CORS
 app.use(express.json());
 app.use("/movies", moviesRouter);
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 app.listen(PORT, () => {
