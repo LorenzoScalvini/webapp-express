5338963865c8c342f1ed745fc1090f8e4eee72d7
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-01-10 15:50:39 +0100

    ADD: fix movies controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,7 +1,7 @@
 const connection = require("../data/db");
 
 function index(req, res) {
-  const sql = "SELECT * FROM movies";
+  const sql = "SELECT id, title, image_url FROM movies";
 
   connection.query(sql, (err, results) => {
     if (err) {
